b9298a16a0d542ca0a5e6654d99aaeed9d258b5c
unreal! <aimaressi88@gmail.com>
2026-05-21 09:36:36 +0200

    Refactor content structure and add cards

--- content.js
+++ content.js
@@ -4,23 +4,38 @@ window.docsContent = {
     {
       id: "test",
       category: "test",
-      blocks: [
-        {type: "title", content: "test"},
-        {type: "subtitle", content: "yeah so this is a test"},
-        {type: "text", content: "ima fill this up with random stuff so it can fill up space, i say fill up so much lol, fill up"},
-        {type: "snippet", label: "fillup.js", content: 'fillup\n   lololol\n   hi'}
+      cards: [
+        {
+          blocks: [
+            {type: "title", content: "test"},
+            {type: "subtitle", content: "yeah so this is a test"},
+            {type: "text", content: "ima fill this up with random stuff so it can fill up space, i say fill up so much lol, fill up"},
+            {type: "snippet", label: "fillup.js", content: "fillup\n   lololol\n   hi"}
+          ]
+        },
+        {
+          blocks: [
+            {type: "title", content: "second test card"},
+            {type: "text", content: "see? two cards in the same category"},
+            {type: "snippet", label: "wow.js", content: "im so fucking cool omgomgomgomg"}
+          ]
+        }
       ]
     },
     {
       id: "how",
       category: "how this works",
-      blocks: [
-        {type: "title", content: "how this works"},
-        {type: "subtitle", content: "explaining how to do this for dummies"},
-        {type: "text", content: "so basically there is a content.js that has all the stuff needed to write all these cards."},
-        {type: "text", content: "there are different placeholders to replace the info like title, subtitle, text, snippet, category and id."},
-        {type: "text", content: "to use enter while writing the text you have to type /n (but actually with the other slash, like the one its otherwise) right where you would click enter."},
-        {type: "snippet", label: "why", content: "uwu"}
+      cards: [
+        {
+          blocks: [
+            {type: "title", content: "how this works"},
+            {type: "subtitle", content: "explaining how to do this for dummies"},
+            {type: "text", content: "so basically there is a content.js that has all the stuff needed to write all these cards."},
+            {type: "text", content: "there are different placeholders to replace the info like title, subtitle, text, snippet, category and id."},
+            {type: "text", content: "to use enter while writing the text you have to type \\n right where you would click enter."},
+            {type: "snippet", label: "why", content: "uwu"}
+          ]
+        }
       ]
     },
 //alr, this is the placeholder, just copy paste it after one of the other categories and change the info
@@ -28,24 +43,34 @@ window.docsContent = {
     {
       id: "placeholder",
       category: "placeholder",
-      blocks: [
-        {type: "title", content: "placeholder"},
-        {type: "subtitle", content: "placeholder"},
-        {type: "text", content: "guess what, placeholder."},
-        {type: "snippet", label: "place", content: "hoooooooooooolder"}
+      cards: [
+        //card start
+        {
+          blocks: [
+            {type: "title", content: "placeholder"},
+            {type: "subtitle", content: "placeholder"},
+            {type: "text", content: "guess what, placeholder."},
+            {type: "snippet", label: "place", content: "hoooooooooooolder"}
+          ]
+        }
+        //card finish (copy paste this right after if you need another card under the same category)
       ]
     },
 //finish
     {
       id: "neocities",
       category: "my web",
-      blocks: [
-        {type: "title", content: "my web ooo"},
-        {type: "subtitle", content: "yeah so basically showing my web"},
-        {type: "text", content: "idk if you already know but im the best front end dev alive so check out my very cool best in the world web on itzmeunreal.neocities.org. yeayeayea go check it out"},
-        {type: "text", content: "alr ima check if i can put two snippets"},
-        {type: "snippet", label: "uno", content: "snippet1"},
-        {type: "snippet", label: "dos", content: "snippet2\n omgomgomg"}
+      cards: [
+        {
+          blocks: [
+            {type: "title", content: "my web ooo"},
+            {type: "subtitle", content: "yeah so basically showing my web"},
+            {type: "text", content: "idk if you already know but im the best front end dev alive so check out my very cool best in the world web on itzmeunreal.neocities.org. yeayeayea go check it out"},
+            {type: "text", content: "alr ima check if i can put two snippets"},
+            {type: "snippet", label: "uno", content: "snippet1"},
+            {type: "snippet", label: "dos", content: "snippet2\n omgomgomg"}
+          ]
+        }
       ]
     }
   ]
