a606a4fca5f405c8833ab90bd0a67a3193d59661
Ace1Ace4767-cmd <Ace1Ace4767@proton.me>
2026-05-21 00:58:12 -0400

    Update content.js - Added Walking forward
    Added first docs for walking forward.

--- content.js
+++ content.js
@@ -2,13 +2,13 @@ window.docsContent = {
   siteTitle: "StupidDocs",
   sections: [
     {
-      id: "test",
-      category: "test",
+      id: "Forward_Movement",
+      category: "Movement",
       blocks: [
-        {type: "title", content: "test"},
-        {type: "subtitle", content: "yeah so this is a test"},
-        {type: "text", content: "ima fill this up with random stuff so it can fill up space, i say fill up so much lol, fill up"},
-        {type: "snippet", label: "fillup.js", content: 'fillup\n   lololol\n   hi'}
+        {type: "title", content: "Walk Forward"},
+        {type: "subtitle", content: "idk yet"},
+        {type: "text", content: "idk yet 2.0"},
+        {type: "snippet", label: "bot.setControlState'forward', True)", content: 'bot.setControlState('forward', True)'}
       ]
     },
     {
